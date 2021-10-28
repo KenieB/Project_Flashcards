6762a08d2121eb917d9a331784e1994c1f06b719
Kenie B <kbett001@gmail.com>
2021-10-28 19:44:22 -0400

    Create Deck working

--- src/Deck/DeckManagement/DeckForm.js
+++ src/Deck/DeckManagement/DeckForm.js
@@ -17,25 +17,20 @@ export const DeckForm = ({ deck = {}, setDeck, setError }) => {
   //Deck Form handlers
   const handleNameChange = (event) => setName(event.target.value);
   const handleDescriptionChange = (event) => setDescription(event.target.value);
-  const handleSubmitNew = (event) => {
-      //event.preventDefault();
-      const abortController = new AbortController();
-      createDeck({name: name, description: description}, abortController.signal).then(setDeck).catch(setError);
-
-  }
   
-  /*useEffect(() => {
-      if(!url === "/decks/new"){
-        history.push(`/decks/${deck.id}`)
-      }
-    
-  })*/
-
+  const handleSubmitNew = (event) => {
+      event.preventDefault();
+      const abortController = new AbortController();
+      const newDeck = {name: name, description: description};
+      createDeck(newDeck, abortController.signal).then(setDeck).catch(setError);
+      history.push(`/decks/${deck.id}`);
+  };  
+ 
 
   if (url === "/decks/new") {
     return (
       <>
-        <form>
+        <form onSubmit={handleSubmitNew}>
           <div className="form-group">
             <label htmlFor="name">Name</label>
             <input
@@ -68,7 +63,7 @@ export const DeckForm = ({ deck = {}, setDeck, setError }) => {
           >
             Cancel
           </button>
-          <button type="submit" className="btn btn-primary ml-1" onSubmit={handleSubmitNew}>
+          <button type="submit" className="btn btn-primary ml-1">
             Submit
           </button>
         </form>
